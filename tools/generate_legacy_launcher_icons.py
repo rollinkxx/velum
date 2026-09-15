ce3dbfcfb9ea59f7e6583c89976516a29c06a831
rollinkxx <1.53582146e+08+rollinkxx@users.noreply.github.com>
2026-09-15 05:10:34 +0000

    Samakan ikon Velum dan buat transparan

--- tools/generate_legacy_launcher_icons.py
+++ tools/generate_legacy_launcher_icons.py
@@ -5,8 +5,7 @@ ROOT = Path(__file__).resolve().parents[1]
 OUT = ROOT / "app/src/main/res"
 DENSITIES = {"mdpi": 48, "hdpi": 72, "xhdpi": 96, "xxhdpi": 144, "xxxhdpi": 192}
 
-BG_TOP = (11, 15, 21, 255)
-BG_BOTTOM = (30, 42, 58, 255)
+TRANSPARENT = (0, 0, 0, 0)
 SILVER_TOP = (247, 248, 250, 255)
 SILVER_BOTTOM = (83, 100, 123, 255)
 BLUE_TOP = (24, 35, 51, 255)
@@ -27,7 +26,7 @@ def vertical_gradient(size, top, bottom, start=0.0, end=1.0):
     return image
 
 def icon(size):
-    im = vertical_gradient(size, BG_TOP, BG_BOTTOM)
+    im = Image.new("RGBA", (size, size), TRANSPARENT)
     s = size / 108.0
     silver_pts = [(22*s,25*s),(37*s,25*s),(54*s,61*s),(60*s,73*s),(56*s,83*s),(49*s,85*s),(43*s,74*s)]
     blue_pts = [(69*s,25*s),(86*s,25*s),(65*s,68*s),(61*s,77*s),(57*s,82*s),(51*s,83*s),(56*s,79*s),(58*s,74*s),(60*s,68*s)]
